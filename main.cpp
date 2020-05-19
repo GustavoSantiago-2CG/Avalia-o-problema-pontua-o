61aff930f083b0137cd98a27e736e16ccdd89fe8
Gustavo Santiago <flymummykgg@gmail.com>
2020-05-19 09:42:42 -0300

    Scores de inadimplencia e pagamento

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main(){
 	
-	double compras, ticketmedio, atraso, pagamento, score, pontos;
+	double compras, ticketmedio, atraso, score, pontos1, pontos2, pontos3;
+	string pagamento, B, C, D;
+	
 	
 	cout <<endl<< "SISTEMA DE PERFIL DE CLIENTE" << endl;
 	cout << "--------------------------------" << endl;
@@ -21,17 +23,46 @@ int main(){
 	cin >> pagamento;
 	
 	if (score == 0){
-		pontos = 0;
+		pontos1 = 0;
 	}
-	if (score < 3000){
-		pontos = 20;
+	
+	if (score <= 3000 && compras <= 2){
+		pontos1 = 20;
 	}
-	if (score == 3000)
-        pontos = 40;
-    if(score > 3000){
-    	pontos = 60;
+	
+	if (score == 3000 && compras >= 2){
+        pontos1 = 40;
+    }
+    
+	else{
+    	pontos1 = 60;
 	}    
 	
-	cout <<endl<<endl<< "Score de volume de compras = "	<< pontos << " pontos" << endl;	
+	cout <<endl<<endl<< "Score de volume de compras = "	<< pontos1 << " pontos" << endl;	
+	
+	if (atraso > 1 || compras == 0);{
+	     pontos2 = 0;
+		 }
+	if (compras >= 1 && atraso == 1){
+		pontos2 = 15;
+	} 
+	if (compras >= 1 && atraso == 0){
+		pontos2 = 30; 
+	}    
+         	
+	cout << "Score de inadimplencia = "	<< 	pontos2 << " pontos" << endl;												
+	
+	if (pagamento == "D" && compras > 0){
+		pontos3 = 5; 
+	}
+	if (pagamento == "C" || pagamento == "B" && compras > 0){
+		pontos3 = 10; 
+	}
+	
+	cout << "Score de forma de pagamento = "<< 	pontos3 << " pontos" << endl;
+	
+	
+	
+	
 	return 0;
 }
