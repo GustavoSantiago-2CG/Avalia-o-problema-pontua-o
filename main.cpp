8e1e66e046ece9f960cd923578c0c179e9d9e8f9
Gustavo Santiago <flymummykgg@gmail.com>
2020-05-19 07:56:31 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 
 int main(){
+	
+	cout <<endl<< "SISTEMA DE PERFIL DE CLIENTE" << endl;
+	cout << "--------------------------------" << endl;
+	cout << "INFORME OS DADOS DO ÚLTIMO ANO" <<endl<<endl;
+	
+	return 0;
+}
